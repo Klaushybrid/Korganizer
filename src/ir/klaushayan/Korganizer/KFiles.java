6481b07f996f6d4ba764254f363c7c9043fddd8b
klaushybrid <klaus_hybrid33@yahoo.com>
2017-04-15 23:40:29 +0430

    bug fix

--- src/ir/klaushayan/Korganizer/KFiles.java
+++ src/ir/klaushayan/Korganizer/KFiles.java
@@ -7,64 +7,60 @@ import java.util.ListIterator;
 
 import javax.swing.JFileChooser;
 
-
-
-
-
 public class KFiles {
 	private static ArrayList<String> Addresses = new ArrayList<>();
 	private static ArrayList<String> RootAddresses = new ArrayList<>();
-	private static ArrayList<String> NamesListString =new ArrayList<>();
+	private static ArrayList<String> NamesListString = new ArrayList<>();
+
 	public static ArrayList<String> getFiles() {
 		JFileChooser chooser = new JFileChooser();
 		chooser.setMultiSelectionEnabled(true);
 		int returnval = chooser.showOpenDialog(null);
-		if (returnval==chooser.APPROVE_OPTION){
-		
-	    ArrayList<File> NamesList = new ArrayList<>(Arrays.asList(chooser.getSelectedFiles()));	
-		ArrayList<String> StringNamesList = new ArrayList<>();
+		if (returnval == chooser.APPROVE_OPTION) {
 
-	    StringNamesList = FiletoString(NamesList);
-		return StringNamesList;
-		}
-		else {
+			ArrayList<File> NamesList = new ArrayList<>(Arrays.asList(chooser.getSelectedFiles()));
+			ArrayList<String> StringNamesList = new ArrayList<>();
+
+			StringNamesList = FiletoString(NamesList);
+			return StringNamesList;
+		} else {
 			return null;
-		
+
 		}
-		
- }
-	private static ArrayList<String> FiletoString(ArrayList<File> NamesList){
+
+	}
+
+	private static ArrayList<String> FiletoString(ArrayList<File> NamesList) {
 		ListIterator litr = NamesList.listIterator();
 		int i = 0;
-		
-		
-		while(litr.hasNext()){
-		int lastindex = NamesList.get(i).toString().lastIndexOf("/");
-		NamesListString.add(NamesList.get(i).toString().replace(NamesList.get(i).toString().substring(0,lastindex+1), ""));
-		Addresses.add(NamesList.get(i).toString());
-		RootAddresses.add(Addresses.get(i).replace(Addresses.get(i).toString().substring(lastindex+1 , Addresses.get(i).toString().length()), ""));
-		litr.next();
-		i++;
-		
+
+		while (litr.hasNext()) {
+			int lastindex = NamesList.get(i).toString().lastIndexOf("/");
+			NamesListString.add(
+					NamesList.get(i).toString().replace(NamesList.get(i).toString().substring(0, lastindex + 1), ""));
+			Addresses.add(NamesList.get(i).toString());
+			RootAddresses.add(Addresses.get(i).replace(
+					Addresses.get(i).toString().substring(lastindex + 1, Addresses.get(i).toString().length()), ""));
+			litr.next();
+			i++;
+
 		}
-		
-		
-		
+
 		return NamesListString;
-		
-		
+
 	}
-	
-	public ArrayList<String> getFileLoc(){
+
+	public ArrayList<String> getFileLoc() {
 		return Addresses;
 	}
-	public ArrayList<String> getRootLoc(){
+
+	public ArrayList<String> getRootLoc() {
 		return RootAddresses;
-		
+
 	}
-	public ArrayList<String> getNames(){
+
+	public ArrayList<String> getNames() {
 		return NamesListString;
 	}
-	
-	
+
 }
--- src/ir/klaushayan/Korganizer/Purification.java
+++ src/ir/klaushayan/Korganizer/Purification.java
@@ -12,145 +12,126 @@ import java.util.concurrent.Future;
 import javax.swing.JOptionPane;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-
-public class Purification implements Callable<ArrayList<String>>{
+public class Purification implements Callable<ArrayList<String>> {
 	public final File XMLFile = new File(getpackname());
 	private ArrayList<String> StringArray;
 	private static ArrayList<String> LoweredString = new ArrayList<>();
-	
-	public Purification(ArrayList<String> StringArray){
+
+	public Purification(ArrayList<String> StringArray) {
 		this.StringArray = StringArray;
-		
+
 	}
-	public ArrayList<String> Purify(){
-		
+
+	public ArrayList<String> Purify() {
+
 		return Purify(false);
 	}
-	
-	
-	
+
 	@SuppressWarnings("unchecked")
-	public ArrayList<String> Purify(boolean ToRemove){
+	public ArrayList<String> Purify(boolean ToRemove) {
 		getpackname();
-		if (StringArray == null)
-		{
+		if (StringArray == null) {
 			return null;
 		}
-	ListIterator litr = StringArray.listIterator();
-	int i =0;
-	while(litr.hasNext()){
-	StringArray.set(i,StringArray.get(i).replace(",", " "));	
-	StringArray.set(i,StringArray.get(i).replace("-", " "));
-	StringArray.set(i,StringArray.get(i).replace("_", " "));
-	StringArray.set(i,StringArray.get(i).replace(StringArray.get(i).substring(StringArray.get(i).lastIndexOf("."), StringArray.get(i).length()), ""));
-	LoweredString.add(StringArray.get(i).toLowerCase());
-	litr.next();
-	i++;
-	}
-	if (ToRemove){
-	try {
-				
-			ExecutorService EXTSERV = Executors.newFixedThreadPool(1);
-			Future<ArrayList<String>> future = EXTSERV.submit(new Purification(StringArray));
-			StringArray = future.get();
-			
-		} catch (Exception e) {
-			System.out.println(e);
+		ListIterator litr = StringArray.listIterator();
+		int i = 0;
+		while (litr.hasNext()) {
+			StringArray.set(i, StringArray.get(i).replace(",", " "));
+			StringArray.set(i, StringArray.get(i).replace("-", " "));
+			StringArray.set(i, StringArray.get(i).replace("_", " "));
+			StringArray.set(i, StringArray.get(i).replace(
+					StringArray.get(i).substring(StringArray.get(i).lastIndexOf("."), StringArray.get(i).length()),
+					""));
+			StringArray.set(i, StringArray.get(i).replace(".", " "));
+			LoweredString.add(StringArray.get(i).toLowerCase());
+			litr.next();
+			i++;
 		}
-	}
-		
+		if (ToRemove) {
+			try {
+
+				ExecutorService EXTSERV = Executors.newFixedThreadPool(1);
+				Future<ArrayList<String>> future = EXTSERV.submit(new Purification(StringArray));
+				StringArray = future.get();
+
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
 		return StringArray;
 	}
-	
-	
-	
-	
-	
-	
-	//Done 
-private ArrayList<String> XMLArrayReader(File XMLFile){
-	SAXReader reader = new SAXReader();
-	ArrayList<String> ToRemove = new ArrayList<>();
-	try{
-	Document doc = reader.read(XMLFile);
-	Element rootel = doc.getRootElement();
-	Iterator itr = rootel.elementIterator();
-	Element itemel =null;
-	while (itr.hasNext()) {
-		itemel = (Element) itr.next(); 
-		ToRemove.add(itemel.getTextTrim().toString());
+
+	// Done
+	private ArrayList<String> XMLArrayReader(File XMLFile) {
+		SAXReader reader = new SAXReader();
+		ArrayList<String> ToRemove = new ArrayList<>();
+		try {
+			Document doc = reader.read(XMLFile);
+			Element rootel = doc.getRootElement();
+			Iterator itr = rootel.elementIterator();
+			Element itemel = null;
+			while (itr.hasNext()) {
+				itemel = (Element) itr.next();
+				ToRemove.add(itemel.getTextTrim().toString());
+			}
+		} catch (Exception docE) {
+			JOptionPane.showMessageDialog(null, "Purifier.xml does not exist, either create one or redownload");
+			System.exit(0);
+
 		}
+		return ToRemove;
 	}
-	catch (DocumentException docE){
-		JOptionPane.showMessageDialog(null,"Purifier.xml does not exist, either create one or redownload");
-		System.exit(0);
-		
 
-	}
-    return ToRemove;
-}
+	public ArrayList<String> call() {
 
-
-
-public ArrayList<String> call(){
-	
-	ListIterator litr = StringArray.listIterator();
-		int i=0;
+		ListIterator litr = StringArray.listIterator();
+		int i = 0;
 		ArrayList<String> XMLString = new ArrayList<>();
 		XMLString = XMLArrayReader(XMLFile);
 
-		
-	
-	while(litr.hasNext()){
-		int j=0;
-		ListIterator litrXML = XMLString.listIterator();
-		while(litrXML.hasNext()){
-	if (LoweredString.get(i).contains(XMLString.get(j))){
-		if ((LoweredString.get(i).contains("(")) && (LoweredString.get(i).indexOf("(") < LoweredString.get(i).indexOf(XMLString.get(j))) && (LoweredString.get(i).indexOf(")") > LoweredString.get(i).indexOf(XMLString.get(j)))){
-			StringArray.set(i, StringArray.get(i).replace(StringArray.get(i).substring(LoweredString.get(i).indexOf("("), StringArray.get(i).length()), "" ));
-			LoweredString.set(i,StringArray.get(i).toLowerCase());
-			
+		while (litr.hasNext()) {
+			int j = 0;
+			ListIterator litrXML = XMLString.listIterator();
+			while (litrXML.hasNext()) {
+				if (LoweredString.get(i).contains(XMLString.get(j))) {
+					if ((LoweredString.get(i).contains("("))
+							&& (LoweredString.get(i).indexOf("(") < LoweredString.get(i).indexOf(XMLString.get(j)))
+							&& (LoweredString.get(i).indexOf(")") > LoweredString.get(i).indexOf(XMLString.get(j)))) {
+						StringArray.set(i, StringArray.get(i).replace(StringArray.get(i)
+								.substring(LoweredString.get(i).indexOf("("), StringArray.get(i).length()), ""));
+						LoweredString.set(i, StringArray.get(i).toLowerCase());
+
+					} else {
+
+						StringArray.set(i, StringArray.get(i).replace(StringArray.get(i).substring(
+								LoweredString.get(i).indexOf(XMLString.get(j)), StringArray.get(i).length()), ""));
+						LoweredString.set(i, StringArray.get(i).toLowerCase());
+
+					}
+					StringArray.set(i, StringArray.get(i).trim());
+				}
+
+				j++;
+				litrXML.next();
+			}
+
+			i++;
+
+			litr.next();
 		}
-		else{
-		
-		StringArray.set(i, StringArray.get(i).replace(StringArray.get(i).substring(LoweredString.get(i).indexOf(XMLString.get(j)), StringArray.get(i).length()), "" ));
-		LoweredString.set(i,StringArray.get(i).toLowerCase());
-		
-		}
-		StringArray.set(i,StringArray.get(i).trim());
-		}
-	
-	j++;
-	litrXML.next();
-		}
-	
-		i++;
-	
-	litr.next();	
+
+		return StringArray;
+
 	}
-	
-	
-	
-	
-	
-	
-	return StringArray;
 
+	private String getpackname() {
+		String string = (Purification.class.getPackage().getName().toString().replace(".", "/")
+				+ "/resources/options/Purifier.xml");
+		return Purification.class.getResource("/" + string).toString().replace("file:", "");
+	}
 
-	
-}	
-
-
-private String getpackname(){
-	String string = (Purification.class.getPackage().getName().toString().replace(".", "/")+"/resources/options/Purifier.xml");
-	return Purification.class.getResource("/"+string).toString().replace("file:", "");
 }
-
-
-	
-}
-
